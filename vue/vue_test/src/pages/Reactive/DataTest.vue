7340ecb816b55eb2eb3623e3c8aa612a0aea0de0
zxfjd3g <258147149@qq.com>
2020-09-24 20:45:34 +0800

    update

---
+++ vue/vue_test/src/pages/Reactive/DataTest.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p>{{msg1}}</p>
+    <p>{{msg2}}</p>
+    <p>{{msg3}}</p>
+
+    <button @click="msg1 += '--'">更新msg1</button>
+    <button @click="msg2 += '++'">更新msg2</button>
+    <button @click="$emit('update:msg3', msg3+'~~')">更新msg3</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTest',
+  props: ['msg3'],
+  data () {
+    this.msg2 = 'ABC'
+    return {
+      msg1: 'abc'
+    }
+  },
+
+  computed: {
+    msgLength () {
+      return this.msg1.length
+    }
+  },
+
+  methods: {
+    update () {
+      this.msg1 += '---'
+      this.msg2 += '+++'
+    }
+  },
+}
+</script>
---
+++ vue/vue_test/src/pages/Reactive/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>msg3: {{msg3}}</p>
+    <hr>
+    <data-test :msg3.sync="msg3"></data-test>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import DataTest from './DataTest'
+  export default {
+    name: 'Reactive',
+
+    data () {
+      return {
+        msg3: 'atguigu'
+      }
+    },
+
+
+    components: {
+      DataTest
+    }
+  }
+</script>
